5911ed6115850e748187ea03f0c9c4393283e2f6
RuslanIvanov <rus_iv@list.ru>
2016-11-26 13:07:41 +0300

    isWokr

--- matrix/matrix.cpp
+++ matrix/matrix.cpp
@@ -4,12 +4,70 @@
 #include <stdlib.h>
 
 using namespace std;
-int ** transpose(const int * /*const*/ * m, unsigned rows, unsigned cols);
+
+int ** create_array2d(const int& rows, const int& cols) 
+{
+   int **m = new int*[rows];
+   for(int i = 0; i < rows; ++i)
+      m[i] = new int [cols];
+   return m;
+}
+
+void destroy(int** m_, const int& rows)
+{
+   for (int i = 0; i < rows; ++i)
+        delete[] m_[i];
+   delete[] m_;
+} 
+
+int ** transpose(const int * const * m, unsigned rows, unsigned cols)
+{
+    	/* ..a[i,j] = a[j,i].. */
+		/*
+		A = {1,2,3} 
+		{4,5,6}  A^T =  {1, 4}
+				{2, 5}
+				{3, 6}	
+		*/
+		/* in mem:
+		A = {1,2,3,4|5,6,7,8}
+	   	AT= {1,5,2,6|3,7,4,8}
+		*/
+
+	/*int **T = new int* [rows];
+	T[0] = new int [rows*cols];
+	for(int i=1;i!=rows;i++)
+	{T[i] = T[i-1]+cols;}*/// error!!
+
+	int **T = create_array2d(cols, rows);
+
+	for(int a=0;a<rows;a++)
+		for(int b=0;b<cols;b++)
+	{
+		T[b][a] = m[a][b];//*(*(m+a)+b);
+	}
+
+	return T;
+}
+
 const int rows = 2;
 const int cols = 3;
 int main(int argc, char *argv[])
 {
-	int m[rows][cols] = { {1,2,3},{4,5,6} };
+	//int m[rows][cols] = { {1,2,3},{4,5,6} };
+
+	int rows =3, cols = 5;
+    	int **m = create_array2d(rows, cols);
+
+	int count = 0;
+    	for (int i = 0; i < rows; ++i) 
+    	{
+         	for (int j = 0; j < cols; ++j)
+         	{
+              		m[i][j] = count;
+              		count++;
+         	}
+    	}
 
 	cout<<"m:"<<endl;
 	
@@ -19,8 +77,8 @@ int main(int argc, char *argv[])
 		cout<<*(p+i);
 	}
 	cout<<endl;
-	int **pm=&m[0];
-	int **T  =  transpose(pm,rows,cols);
+
+	int **T  =  transpose(m,rows,cols);
 
 	cout<<"mT:"<<endl<<endl;
 	for(int i=0;i<rows;i++)
@@ -30,40 +88,13 @@ int main(int argc, char *argv[])
 		cout<<endl;		
 	}
 
-	delete [] T[0];
-	delete [] T;
+
+	destroy(m, rows);
+    	destroy(T, cols);
 
 	cout<<endl<<"Press any key...";	
 	getchar();
 	return 0;
 }
 
-int ** transpose(const int * /*const*/ * m, unsigned rows, unsigned cols)
-{
-    	/* ..a[i,j] = a[j,i].. */
-		/*
-		A = {1,2,3} 
-			{4,5,6}  A^T =  {1, 4}
-						    {2, 5}
-							{3, 6}	
-		*/
-	/* in mem:
-		A = {1,2,3,4|5,6,7,8}
-	   AT= {1,5,2,6|3,7,4,8}
-	*/
 
-	int **T = new int* [rows];
-	T[0] = new int [rows*cols];
-	for(int i=1;i!=rows;i++)
-	{T[i] = T[i-1]+cols;}
-
-	for(int a=0;a<rows;a++)
-		for(int b=0;b<cols;b++)
-	{
-		T[b][a] = *(*(m+a)+b);
-	}
-
-	return T;
-
-	
-}

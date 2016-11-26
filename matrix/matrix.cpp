f04af7e333bd0acc796b8be2b5d7cac32e1bdcb4
RuslanIvanov <rus_iv@list.ru>
2016-11-26 22:28:40 +0300

    new swap

--- matrix/matrix.cpp
+++ matrix/matrix.cpp
@@ -26,25 +26,20 @@ int ** transpose(const int * const * m, unsigned rows, unsigned cols)
 		/*
 		A = {1,2,3} 
 		{4,5,6}  A^T =  {1, 4}
-				{2, 5}
-				{3, 6}	
+						{2, 5}
+						{3, 6}	
 		*/
-		/* in mem:
+		/*in mem:
 		A = {1,2,3,4|5,6,7,8}
 	   	AT= {1,5,2,6|3,7,4,8}
 		*/
-
-	/*int **T = new int* [rows];
-	T[0] = new int [rows*cols];
-	for(int i=1;i!=rows;i++)
-	{T[i] = T[i-1]+cols;}*/// error!!
 
 	int **T = create_array2d(cols, rows);
 
 	for(int a=0;a<rows;a++)
 		for(int b=0;b<cols;b++)
 	{
-		T[b][a] = m[a][b];//*(*(m+a)+b);
+		T[b][a] = m[a][b];
 	}
 
 	return T;
@@ -54,20 +49,18 @@ const int rows = 2;
 const int cols = 3;
 int main(int argc, char *argv[])
 {
-	//int m[rows][cols] = { {1,2,3},{4,5,6} };
-
 	int rows =3, cols = 5;
-    	int **m = create_array2d(rows, cols);
+    int **m = create_array2d(rows, cols);
 
 	int count = 0;
-    	for (int i = 0; i < rows; ++i) 
-    	{
-         	for (int j = 0; j < cols; ++j)
-         	{
-              		m[i][j] = count;
-              		count++;
-         	}
-    	}
+    for (int i = 0; i < rows; ++i) 
+    {
+       	for (int j = 0; j < cols; ++j)
+       	{
+           		m[i][j] = count;
+           		count++;
+       	}
+    }
 
 	cout<<"m:"<<endl;
 	
@@ -90,7 +83,7 @@ int main(int argc, char *argv[])
 
 
 	destroy(m, rows);
-    	destroy(T, cols);
+    destroy(T, cols);
 
 	cout<<endl<<"Press any key...";	
 	getchar();
---
+++ swap_min/swap_min.cpp
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int ** create_array2d(const int& rows, const int& cols) 
+{
+	int **m = new int* [rows];
+	m[0] = new int [rows*cols];
+	for(int i=1;i!=rows;i++)
+	{m[i] = m[i-1]+cols;}
+
+	return m;
+}
+
+void destroy(int** m_, const int& rows)
+{
+	delete[] m_[0];
+	delete[] m_;
+} 
+
+void swap_min(int *m[], unsigned rows, unsigned cols)
+{
+    /* ... */
+	int min=m[0][0];
+	int rmin=0;
+	for (int i = 0; i < rows; i++) 
+    {
+       	for (int j = 0; j < cols; j++)
+       	{
+				if(m[i][j]<min){min = m[i][j]; rmin=i;}
+		}
+    }
+
+	cout<<"addr: "<<m[0]<<" "<<m[rmin]<<endl;
+	//int *p=&m[0][0];
+	//m[0] = &m[rmin][0];
+	int *p=m[0];
+	m[0] = m[rmin];
+	m[rmin]=p;
+	cout<<"addr: "<<m[0]<<" "<<m[rmin]<<endl;
+	cout<<"min="<<min<<" rmin="<<rmin<<endl;
+	
+
+}
+
+int rows;
+int cols;
+int main(int argc, char *argv[])
+{
+	srand(time(NULL));
+	rows=rand()%5;
+	cols=rand()%5;
+
+	if(!rows) rows = 2;
+	if(!cols) cols = 3;
+
+	int **m = create_array2d(rows, cols);
+	
+    for (int i = 0; i < rows; ++i) 
+    {
+       	for (int j = 0; j < cols; ++j)
+       	{m[i][j] = rand()%10;}
+    }
+
+	cout<<"m"<<"["<<rows<<","<<cols<<"]"<<endl;
+	
+	int *p = m[0];
+	for(int i=0,j=0;i<rows*cols;i++)
+	{cout<<*(p+i)<<" "; if(++j==cols){j=0; cout<<endl;}} cout<<endl;
+
+	swap_min(m,rows,cols);
+
+	cout<<"mnew:"<<endl<<endl;
+	p = m[0];
+	for(int i=0,j=0;i<rows*cols;i++)
+	{cout<<*(p+i)<<" "; if(++j==cols) {j=0; cout<<endl; } }cout<<endl;
+	
+	destroy(m, rows);
+    
+	cout<<endl<<"Press any key...";	
+	getchar();
+	return 0;
+}
+
+

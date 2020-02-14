cc48de343c43449edb0fe8b92803b5732e95dba1
RuslanIvanov <rus_iv@list.ru>
2020-02-14 09:56:07 +0300

    templ

--- template/tecmpl.cpp
+++ template/tecmpl.cpp
@@ -2,13 +2,13 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-template<typename T> void foo(T) { std::cout<<"1"; }  // 1
-template<> void foo(int*) { std::cout<<"2";  }         // 2
-template<typename T> void foo(T*){ std::cout<<"3"; } // 3
+template<typename T> void foo(T) { std::cout<<" 1 "; }  // 1
+template<> void foo(int*) { std::cout<<" 2 ";  }         // 2
+template<typename T> void foo(T*){ std::cout<<" 3 "; } // 3
 
-template<typename T> void bar(T){ std::cout<<"4"; }  // 4
-template<typename T> void bar(T*){std::cout<<"5"; } // 5
-template<> void bar(int*){ std::cout<<"6"; }         // 6
+template<typename T> void bar(T){ std::cout<<" 4 "; }  // 4
+template<typename T> void bar(T*){std::cout<<" 5 "; } // 5
+template<> void bar(int*){ std::cout<<" 6 "; }         // 6
 
 void f();
 

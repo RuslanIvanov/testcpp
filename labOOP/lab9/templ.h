8730ccbbea475b958cbe90174f3c05bd11d2723e
RuslanIvanov <rus_iv@list.ru>
2020-02-14 10:05:14 +0300

    ttt

--- labOOP/lab9/templ.h
+++ labOOP/lab9/templ.h
@@ -10,13 +10,15 @@ template <typename T> void printCont(const T& t)
     }
 
 }
-
-template <class T> void printCont<queue>(const T& t)
+//template <typename Q>
+typename Q;
+template <> void printCont<std::queue<Q>>(const <std::queue<Q>>& t)
 {
-    std::cout<<"\nContainer:";
-    for(size_t i = 0; i < t.size(); i++)
+    std::cout<<"\nContainer queue:";
+    while(!t.empty()) 
     {
         std::cout<<t.top();
+	t.pop()
     }
 
 }

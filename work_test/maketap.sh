cd2e314fc9fd961ae3e8e4a769f1989ce542b8c2
RuslanIvanov <rus_iv@list.ru>
2020-07-19 00:45:32 +0300

    tup

--- work_test/maketap.sh
+++ work_test/maketap.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
+ip tuntap add dev tap2 mode tap
+ip address add 192.168.9.222/24 dev tap2
+ip address show tap2
+
+ifconfig tap2 up
+
+
+ip tuntap add dev tap3 mode tap
+ip address add 192.168.8.222/24 dev tap3
+ip address show tap3
+
+ifconfig tap3 up
+
+
 ip tuntap add dev tap0 mode tap
-ip address add 192.168.9.222/24 dev tap0
+ip address add 192.168.0.222/24 dev tap0
 ip address show tap0
 
 ifconfig tap0 up
 
-
 ip tuntap add dev tap1 mode tap
-ip address add 192.168.8.222/24 dev tap1
+ip address add 192.168.2.222/24 dev tap1
 ip address show tap1
 
 ifconfig tap1 up
+

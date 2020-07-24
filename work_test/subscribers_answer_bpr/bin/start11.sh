2742a4493cef6edd26bfc3883c433bbcb1a761bc
RuslanIvanov <rus_iv@list.ru>
2020-07-24 08:34:55 +0300

    fff

--- work_test/subscribers_answer_bpr/bin/start11.sh
+++ work_test/subscribers_answer_bpr/bin/start11.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo ./upu_answ -a 11 -s -e tap0 -E tap1
-
+#sudo ./upu_answ -a 11 -s -e tap0 -E tap1
+sudo ./upu_answ -a 11 -s -D 40 -e enp3s0 -E tap1
 
 
 
--- work_test/subscribers_answer_bpr/bin/start13.sh
+++ work_test/subscribers_answer_bpr/bin/start13.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #50 - не успевает
 #sudo ./upu_answ -a 13 -s -D 50
-sudo ./upu_answ -a 13 -s -D 30
+#sudo ./upu_answ -a 13 -s -D 30
+sudo ./upu_answ -a 13 -s -D 40 -e enp3s0 -E tap1
+#sudo ./upu_answ -a 13 -s -D 40 -e enp3s0 -E tap1
 
 
 
